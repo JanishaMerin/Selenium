e476c160f8caade0601a8dd60c44b359081d29e4
JanishaMerin <125045737+JanishaMerin@users.noreply.github.com>
2023-02-12 16:29:50 +0530

    Add files via upload

---
+++ TwoWheeler.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 4;
+		short m = 2;
+		long c = 2134567890344545l;
+		boolean punctured = true;
+		String bikename =("Scooty");
+		double k = 43.3;
+		System.out.println("Number of wheels is" +n);
+		System.out.println("NUmber of mirrors is" +m);
+		System.out.println("Chassis number is" +c);
+		System.out.println("bike condition punctured is true");
+		System.out.println("Bike name is Scooty");
+		System.out.println("current km is" +k);
+		
+
+	}
+
+}
